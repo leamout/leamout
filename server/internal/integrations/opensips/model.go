e3cb46f58e2c0f13c5c433e2acccfd890d31a5a7
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-25 16:30:12 +0000

    feat(opensips): add integration models

--- server/internal/integrations/opensips/model.go
+++ server/internal/integrations/opensips/model.go
@@ -1,3 +1,34 @@
 package opensips
 
-// Define OpenSIPS models here
+import "time"
+
+type Config struct {
+	URL            string
+	ConnectTimeout time.Duration
+	RequestTimeout time.Duration
+}
+
+func DefaultConfig(url string) Config {
+	return Config{
+		URL:            url,
+		ConnectTimeout: 5 * time.Second,
+		RequestTimeout: 5 * time.Second,
+	}
+}
+
+type Command struct {
+	Name   string
+	Params map[string]string
+}
+
+type Response struct {
+	Code    int
+	Message string
+	Params  map[string]string
+}
+
+type Event struct {
+	Name      string
+	Timestamp time.Time
+	Params    map[string]string
+}
