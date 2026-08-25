78dff6490c921640ecd8a3e97f11aefc5c37c865
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-25 14:28:20 +0000

    fix(worker): add executable entrypoint

--- server/cmd/worker/main.go
+++ server/cmd/worker/main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"log"
+
+	"github.com/leamout/leamout/server/internal/runtime/worker"
+)
+
+func main() {
+	w := worker.New()
+	if err := w.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
