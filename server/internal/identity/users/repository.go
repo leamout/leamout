9012beea5c28359067923c735103cc9f21ccd8d0
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-26 17:10:15 +0000

    Implement users repository

--- server/internal/identity/users/repository.go
+++ server/internal/identity/users/repository.go
@@ -1,1 +1,24 @@
 package users
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/leamout/leamout/internal/database/sqlc"
+)
+
+type Repository struct {
+	queries *sqlc.Queries
+}
+
+func NewRepository(queries *sqlc.Queries) *Repository {
+	return &Repository{queries: queries}
+}
+
+func (r *Repository) GetByID(ctx context.Context, id uuid.UUID) (sqlc.User, error) {
+	return r.queries.GetUserByID(ctx, id)
+}
+
+func (r *Repository) UpdateProfile(ctx context.Context, arg sqlc.UpdateUserProfileParams) (sqlc.User, error) {
+	return r.queries.UpdateUserProfile(ctx, arg)
+}
