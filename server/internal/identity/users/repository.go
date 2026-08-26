4c7c41ac233484b224f9e61c2bd0eeeb6a52216f
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-26 17:31:38 +0000

    Add user disable repository operation

--- server/internal/identity/users/repository.go
+++ server/internal/identity/users/repository.go
@@ -22,3 +22,7 @@ func (r *Repository) GetByID(ctx context.Context, id uuid.UUID) (sqlc.User, erro
 func (r *Repository) UpdateProfile(ctx context.Context, arg sqlc.UpdateUserProfileParams) (sqlc.User, error) {
 	return r.queries.UpdateUserProfile(ctx, arg)
 }
+
+func (r *Repository) Disable(ctx context.Context, id uuid.UUID) error {
+	return r.queries.DisableUser(ctx, id)
+}
