949a968b3731617e98b93555ab878c60339a3eb3
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-26 17:10:36 +0000

    Add users validation

--- server/internal/identity/users/validation.go
+++ server/internal/identity/users/validation.go
@@ -1,1 +1,23 @@
 package users
+
+import (
+	"strings"
+
+	"github.com/leamout/leamout/pkg/apperror"
+)
+
+func validateUpdateProfileRequest(req UpdateProfileRequest) error {
+	if req.Name == nil {
+		return apperror.NewBadRequest("name is required")
+	}
+
+	if strings.TrimSpace(*req.Name) == "" {
+		return apperror.NewBadRequest("name cannot be empty")
+	}
+
+	if len([]rune(strings.TrimSpace(*req.Name))) > 200 {
+		return apperror.NewBadRequest("name is too long")
+	}
+
+	return nil
+}
