e1f421b12f9d0e6f060f4702c83ed26604cf9800
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-26 17:10:10 +0000

    Implement users API models

--- server/internal/identity/users/model.go
+++ server/internal/identity/users/model.go
@@ -1,1 +1,22 @@
 package users
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+// Response is the public representation of an authenticated user.
+// Authentication secrets such as password hashes are intentionally omitted.
+type Response struct {
+	ID            uuid.UUID `json:"id"`
+	Email         string    `json:"email"`
+	EmailVerified bool      `json:"email_verified"`
+	Name          *string   `json:"name,omitempty"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
+}
+
+type UpdateProfileRequest struct {
+	Name *string `json:"name"`
+}
