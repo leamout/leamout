ef722ddd3f115f8f84a2cecf26f462e0a073785f
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-26 07:54:26 +0000

    refactor(helper): add reusable HTTP helpers

--- server/pkg/helper/helper.go
+++ server/pkg/helper/helper.go
@@ -1,10 +1,12 @@
 package helper
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/leamout/leamout/internal/database/pgconv"
+	"github.com/leamout/leamout/pkg/apperror"
 )
 
 func FormatTime(value pgtype.Timestamptz) string {
@@ -18,4 +20,32 @@ func FormatTime(value pgtype.Timestamptz) string {
 	}
 
 	return t.Format(http.TimeFormat)
+}
+
+func DecodeJSON[T any](r *http.Request) (T, error) {
+	var value T
+
+	if err := json.NewDecoder(r.Body).Decode(&value); err != nil {
+		return value, apperror.NewBadRequest("invalid request body")
+	}
+
+	return value, nil
+}
+
+func ClientIP(r *http.Request) *string {
+	value := r.RemoteAddr
+	if value == "" {
+		return nil
+	}
+
+	return &value
+}
+
+func UserAgent(r *http.Request) *string {
+	value := r.Header.Get("User-Agent")
+	if value == "" {
+		return nil
+	}
+
+	return &value
 }
