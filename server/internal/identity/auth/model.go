4ee7ce4165d2f26b703fd3907a44aa18584ec28b
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-26 07:39:25 +0000

    fix(auth): keep request UUIDs as strings

--- server/internal/identity/auth/model.go
+++ server/internal/identity/auth/model.go
@@ -2,35 +2,31 @@ package auth
 
 import "github.com/google/uuid"
 
-// -----------------------------------------------------------------------------
 // Requests
-// -----------------------------------------------------------------------------
 
 type startRequest struct {
 	Email string `json:"email"`
 }
 
 type passwordLoginRequest struct {
-	TransactionID uuid.UUID `json:"transaction_id"`
-	Password      string    `json:"password"`
+	TransactionID string `json:"transaction_id"`
+	Password      string `json:"password"`
 }
 
 type sendOTPRequest struct {
-	TransactionID uuid.UUID `json:"transaction_id"`
+	TransactionID string `json:"transaction_id"`
 }
 
 type verifyOTPRequest struct {
-	TransactionID uuid.UUID `json:"transaction_id"`
-	Code          string    `json:"code"`
+	TransactionID string `json:"transaction_id"`
+	Code          string `json:"code"`
 }
 
 type setPasswordRequest struct {
 	Password string `json:"password"`
 }
 
-// -----------------------------------------------------------------------------
 // Responses
-// -----------------------------------------------------------------------------
 
 type startResponse struct {
 	TransactionID uuid.UUID `json:"transaction_id"`
@@ -39,7 +35,7 @@ type startResponse struct {
 
 type authenticationResponse struct {
 	UserID        uuid.UUID `json:"user_id"`
-	SessionExpiry string    `json:"session_expires_at"`
+	SessionExpiry string    `json:"session_expires_at,omitempty"`
 }
 
 type sendOTPResponse struct {
