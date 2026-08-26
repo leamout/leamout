b09f59bceb32ef1955844a5604ecc30876bd5174
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-26 17:10:44 +0000

    Implement users routes

--- server/internal/identity/users/routes.go
+++ server/internal/identity/users/routes.go
@@ -1,1 +1,20 @@
 package users
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func RegisterRoutes(
+	router chi.Router,
+	handler *Handler,
+	authMiddleware func(http.Handler) http.Handler,
+) {
+	router.Route("/users", func(r chi.Router) {
+		r.Use(authMiddleware)
+
+		r.Get("/me", handler.Current)
+		r.Patch("/me", handler.UpdateProfile)
+	})
+}
