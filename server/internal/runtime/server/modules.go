8e31c335a46960a5300f59086272a51667b5aa7c
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-26 17:36:25 +0000

    Register users module in server

--- server/internal/runtime/server/modules.go
+++ server/internal/runtime/server/modules.go
@@ -3,12 +3,14 @@ package server
 import (
 	"github.com/leamout/leamout/internal/identity/auth"
 	"github.com/leamout/leamout/internal/identity/session"
+	"github.com/leamout/leamout/internal/identity/users"
 	"github.com/leamout/leamout/internal/runtime/middleware"
 )
 
 type Modules struct {
 	Auth    AuthModule
 	Session SessionModule
+	Users   UsersModule
 	Authn   *middleware.AuthnMiddleware
 }
 
@@ -22,4 +24,10 @@ type SessionModule struct {
 	Repository *session.Repository
 	Service    *session.Service
 	Handler    *session.Handler
+}
+
+type UsersModule struct {
+	Repository *users.Repository
+	Service    *users.Service
+	Handler    *users.Handler
 }
