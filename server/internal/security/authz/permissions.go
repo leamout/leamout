fb084b4be48f328b3526162ad861a787af422d60
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-27 10:03:48 +0000

    Refine authorization permissions

--- server/internal/security/authz/permissions.go
+++ server/internal/security/authz/permissions.go
@@ -7,8 +7,31 @@ const (
 	PermissionOrganizationRead   Permission = "organization:read"
 	PermissionOrganizationUpdate Permission = "organization:update"
 	PermissionOrganizationDelete Permission = "organization:delete"
-	PermissionMembersRead        Permission = "members:read"
-	PermissionMembersWrite       Permission = "members:write"
-	PermissionCredentialsRead    Permission = "credentials:read"
-	PermissionCredentialsWrite   Permission = "credentials:write"
+
+	PermissionMembersRead   Permission = "members:read"
+	PermissionMembersAdd    Permission = "members:add"
+	PermissionMembersUpdate Permission = "members:update"
+	PermissionMembersRemove Permission = "members:remove"
+
+	PermissionCredentialsRead   Permission = "credentials:read"
+	PermissionCredentialsCreate Permission = "credentials:create"
+	PermissionCredentialsRevoke Permission = "credentials:revoke"
 )
+
+func (p Permission) IsValid() bool {
+	switch p {
+	case PermissionOrganizationRead,
+		PermissionOrganizationUpdate,
+		PermissionOrganizationDelete,
+		PermissionMembersRead,
+		PermissionMembersAdd,
+		PermissionMembersUpdate,
+		PermissionMembersRemove,
+		PermissionCredentialsRead,
+		PermissionCredentialsCreate,
+		PermissionCredentialsRevoke:
+		return true
+	default:
+		return false
+	}
+}
