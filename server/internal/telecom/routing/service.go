937a12857fcd6168e70567d8bf0e1fe54f3d95cd
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-28 22:50:48 +0000

    feat(routing): add route resolver service

--- server/internal/telecom/routing/service.go
+++ server/internal/telecom/routing/service.go
@@ -1,3 +1,32 @@
 package routing
 
-// Define routing service methods here
+import "context"
+
+type Service struct {
+	resolver *Resolver
+}
+
+func NewService(resolver *Resolver) *Service {
+	return &Service{resolver: resolver}
+}
+
+func (s *Service) ResolveOutbound(
+	ctx context.Context,
+	req OutboundRequest,
+) (OutboundDecision, error) {
+	if err := validateOutboundRequest(req); err != nil {
+		return OutboundDecision{}, err
+	}
+	return s.resolver.resolveOutbound(ctx, req)
+}
+
+func (s *Service) ResolveInbound(
+	ctx context.Context,
+	req InboundRequest,
+) (InboundDecision, error) {
+	sourceIP, err := validateInboundRequest(req)
+	if err != nil {
+		return InboundDecision{}, err
+	}
+	return s.resolver.resolveInbound(ctx, req, sourceIP)
+}
