c2cbbde5b92d8704b3564be729f27e31aee47f33
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-28 12:34:23 +0000

    refactor(telecom): use shared JSON decoder in numbers handler

--- server/internal/telecom/numbers/handler.go
+++ server/internal/telecom/numbers/handler.go
@@ -1,13 +1,13 @@
 package numbers
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/google/uuid"
 	"github.com/leamout/leamout/internal/runtime/middleware"
 	"github.com/leamout/leamout/pkg/apperror"
+	"github.com/leamout/leamout/pkg/helper"
 	"github.com/leamout/leamout/pkg/httputil"
 )
 
@@ -26,8 +26,8 @@ func (h *Handler) Create(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var req CreateRequest
-	if err := decode(r, &req); err != nil {
+	req, err := helper.DecodeJSON[CreateRequest](r)
+	if err != nil {
 		httputil.Error(w, err)
 		return
 	}
@@ -85,8 +85,8 @@ func (h *Handler) Update(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var req UpdateRequest
-	if err := decode(r, &req); err != nil {
+	req, err := helper.DecodeJSON[UpdateRequest](r)
+	if err != nil {
 		httputil.Error(w, err)
 		return
 	}
@@ -136,12 +136,4 @@ func ids(r *http.Request) (uuid.UUID, uuid.UUID, error) {
 	}
 
 	return organizationID, id, nil
-}
-
-func decode(r *http.Request, target any) error {
-	if err := json.NewDecoder(r.Body).Decode(target); err != nil {
-		return apperror.NewBadRequest("invalid request body")
-	}
-
-	return nil
 }
