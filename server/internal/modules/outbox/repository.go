53decf11c1ee94de4b1c5563ea5ba3d84b3ef0e3
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-29 05:53:46 +0000

    feat(outbox): add publisher persistence operations

--- server/internal/modules/outbox/repository.go
+++ server/internal/modules/outbox/repository.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5"
 	"github.com/leamout/leamout/internal/database/sqlc"
 )
@@ -40,4 +41,31 @@ func (r *Repository) Insert(ctx context.Context, event Event) (sqlc.OutboxEvent,
 		Headers:       headers,
 		AvailableAt:   event.AvailableAt,
 	})
+}
+
+func (r *Repository) ClaimPending(ctx context.Context, workerID string, batchSize int32) ([]sqlc.OutboxEvent, error) {
+	return r.queries.ClaimPendingEvents(ctx, sqlc.ClaimPendingEventsParams{
+		LockedBy:  &workerID,
+		BatchSize: batchSize,
+	})
+}
+
+func (r *Repository) MarkPublished(ctx context.Context, id uuid.UUID) error {
+	return r.queries.MarkEventPublished(ctx, id)
+}
+
+func (r *Repository) MarkFailed(ctx context.Context, id uuid.UUID, cause error, retryDelaySeconds int32) error {
+	message := ""
+	if cause != nil {
+		message = cause.Error()
+	}
+	return r.queries.MarkEventFailed(ctx, sqlc.MarkEventFailedParams{
+		LastError:         &message,
+		RetryDelaySeconds: retryDelaySeconds,
+		ID:                id,
+	})
+}
+
+func (r *Repository) ReleaseStaleLocks(ctx context.Context, lockTimeoutSeconds int32) error {
+	return r.queries.ReleaseStaleLocks(ctx, lockTimeoutSeconds)
 }
