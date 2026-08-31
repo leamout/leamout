d33800b5fc5bd8b1b70e8bec508bf8ff99327884
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-31 12:06:23 +0000

    Test catalog price availability semantics

--- server/internal/commercial/catalog/validation_test.go
+++ server/internal/commercial/catalog/validation_test.go
@@ -3,6 +3,7 @@ package catalog
 import (
 	"errors"
 	"testing"
+	"time"
 
 	"github.com/google/uuid"
 )
@@ -27,5 +28,28 @@ func TestNormalizeCodeRejectsWhitespace(t *testing.T) {
 func TestNormalizeIDRequiresID(t *testing.T) {
 	if err := normalizeID(uuid.Nil); !errors.Is(err, ErrIDRequired) {
 		t.Fatalf("normalizeID() error = %v, want %v", err, ErrIDRequired)
+	}
+}
+
+func TestPriceEffectiveAt(t *testing.T) {
+	t.Parallel()
+
+	start := time.Date(2026, 8, 31, 12, 0, 0, 0, time.UTC)
+	end := start.Add(30 * 24 * time.Hour)
+	price := Price{Active: true, EffectiveFrom: start, EffectiveUntil: &end}
+
+	if price.EffectiveAt(start.Add(-time.Second)) {
+		t.Fatal("price must not be available before effective_from")
+	}
+	if !price.EffectiveAt(start) {
+		t.Fatal("price must be available at effective_from")
+	}
+	if price.EffectiveAt(end) {
+		t.Fatal("price must not be available at effective_until")
+	}
+
+	price.Active = false
+	if price.EffectiveAt(start) {
+		t.Fatal("inactive price must not be available")
 	}
 }
