9c93cbe54339c2c13199a05741dd2be6b8044bb6
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-09-02 11:52:29 +0000

    feat(coturn): add internal TURN runtime address

--- server/internal/platform/config/config.go
+++ server/internal/platform/config/config.go
@@ -19,6 +19,7 @@ type Config struct {
 	FreeSWITCHESLAddress  string   `env:"FREESWITCH_ESL_ADDRESS" envDefault:"127.0.0.1:8021"`
 	FreeSWITCHESLPassword string   `env:"FREESWITCH_ESL_PASSWORD,required"`
 	CarrierCredentialKey  string   `env:"CARRIER_CREDENTIAL_ENCRYPTION_KEY,required"`
+	TURNAddress           string   `env:"TURN_ADDRESS" envDefault:"127.0.0.1:3478"`
 	TURNAuthSecret        string   `env:"TURN_AUTH_SECRET,required"`
 	TURNPublicURLs        []string `env:"TURN_PUBLIC_URLS" envSeparator:"," envDefault:"stun:localhost:3478,turn:localhost:3478?transport=udp,turn:localhost:3478?transport=tcp"`
 	CORSOrigins           []string `env:"CORS_ORIGINS" envSeparator:"," envDefault:"http://localhost:3000,http://127.0.0.1:3000"`
