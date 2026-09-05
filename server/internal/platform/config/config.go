fe09815bbf748fe869cb2d161ccf0554965ca6e2
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-09-05 19:56:56 +0000

    style: format managed ingress config

--- server/internal/platform/config/config.go
+++ server/internal/platform/config/config.go
@@ -11,9 +11,9 @@ import (
 )
 
 type DIDWWConfig struct {
-	APIKey        string   `env:"API_KEY"`
-	APIBaseURL    string   `env:"API_BASE_URL" envDefault:"https://api.didww.com/v3"`
-	SourceCIDRs   []string `env:"SOURCE_CIDRS" envSeparator:","`
+	APIKey       string   `env:"API_KEY"`
+	APIBaseURL   string   `env:"API_BASE_URL" envDefault:"https://api.didww.com/v3"`
+	SourceCIDRs  []string `env:"SOURCE_CIDRS" envSeparator:","`
 	SIPEndpoints []string `env:"SIP_ENDPOINTS" envSeparator:","`
 }
 
@@ -33,8 +33,8 @@ type Config struct {
 	FreeSWITCHESLAddress  string      `env:"FREESWITCH_ESL_ADDRESS" envDefault:"127.0.0.1:8021"`
 	FreeSWITCHESLPassword string      `env:"FREESWITCH_ESL_PASSWORD,required"`
 	CarrierCredentialKey  string      `env:"CARRIER_CREDENTIAL_ENCRYPTION_KEY,required"`
-	DIDWW                  DIDWWConfig `envPrefix:"DIDWW_"`
-	SIP                    SIPConfig   `envPrefix:"SIP_"`
+	DIDWW                 DIDWWConfig `envPrefix:"DIDWW_"`
+	SIP                   SIPConfig   `envPrefix:"SIP_"`
 	TURNAuthSecret        string      `env:"TURN_AUTH_SECRET,required"`
 	TURNPublicURLs        []string    `env:"TURN_PUBLIC_URLS" envSeparator:"," envDefault:"stun:localhost:3478,turn:localhost:3478?transport=udp,turn:localhost:3478?transport=tcp"`
 	CORSOrigins           []string    `env:"CORS_ORIGINS" envSeparator:"," envDefault:"http://localhost:3000,http://127.0.0.1:3000"`
