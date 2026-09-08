8735bc5ea613bbd4d4402c53e0032f1d0495274f
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-09-08 15:53:26 +0000

    Remove managed inbound edge feature flag

--- deploy/opensips/entrypoint.sh
+++ deploy/opensips/entrypoint.sh
@@ -5,7 +5,6 @@ config=${OPENSIPS_CONFIG:-/etc/opensips/opensips.cfg}
 advertised_address=${OPENSIPS_ADVERTISED_ADDRESS:-}
 database_password=${OPENSIPS_DATABASE_PASSWORD:-}
 admission_secret=${MANAGED_SIP_ADMISSION_SECRET:-}
-managed_inbound_edge=${MANAGED_INBOUND_EDGE_ENABLED:-false}
 
 [ -n "$database_password" ] || {
   echo "OPENSIPS_DATABASE_PASSWORD is required" >&2
@@ -16,11 +15,6 @@ case "$database_password" in
     echo "OPENSIPS_DATABASE_PASSWORD must be alphanumeric" >&2
     exit 1
     ;;
-esac
-
-case "$managed_inbound_edge" in
-  true|false) ;;
-  *) echo "MANAGED_INBOUND_EDGE_ENABLED must be true or false" >&2; exit 1 ;;
 esac
 
 [ -n "$admission_secret" ] || {
@@ -44,11 +38,6 @@ rm -f "$tmp"
 
 tmp=$(mktemp)
 sed "s#__MANAGED_SIP_ADMISSION_SECRET__#${admission_secret}#g" "$config" > "$tmp"
-cat "$tmp" > "$config"
-rm -f "$tmp"
-
-tmp=$(mktemp)
-sed "s#__MANAGED_INBOUND_EDGE_ENABLED__#${managed_inbound_edge}#g" "$config" > "$tmp"
 cat "$tmp" > "$config"
 rm -f "$tmp"
 
