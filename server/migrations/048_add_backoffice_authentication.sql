e3817b57fa8d84710c876aedb0f8f6bd0cd62f50
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-09-09 21:30:16 +0000

    Remove folded Backoffice authentication migration

--- server/migrations/048_add_backoffice_authentication.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE users
-ADD COLUMN is_platform_admin BOOLEAN NOT NULL DEFAULT false;
-
-ALTER TABLE sessions
-ADD COLUMN audience TEXT NOT NULL DEFAULT 'api';
-
-ALTER TABLE sessions
-ADD CONSTRAINT chk_sessions_audience
-CHECK (audience IN ('api', 'backoffice'));
