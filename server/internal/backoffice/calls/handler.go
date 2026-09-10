0ebf82e2ea44bc5da982d4e005aea4169f3e10b8
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-09-10 09:19:19 +0000

    Build Backoffice call details

--- server/internal/backoffice/calls/handler.go
+++ server/internal/backoffice/calls/handler.go
@@ -2,17 +2,45 @@ package calls
 
 import (
 	"bytes"
+	"context"
+	"errors"
 	"net/http"
 
 	"github.com/a-h/templ"
+	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 )
 
-type Handler struct {
-	repository *Repository
+type repository interface {
+	List(context.Context) ([]Call, error)
+	Get(context.Context, uuid.UUID) (Detail, error)
 }
 
-func NewHandler(repository *Repository) *Handler {
+type Handler struct {
+	repository repository
+}
+
+func NewHandler(repository repository) *Handler {
 	return &Handler{repository: repository}
+}
+
+func (h *Handler) detail(w http.ResponseWriter, r *http.Request) {
+	callID, err := uuid.Parse(chi.URLParam(r, "call_id"))
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+	call, err := h.repository.Get(r.Context(), callID)
+	if errors.Is(err, pgx.ErrNoRows) {
+		http.NotFound(w, r)
+		return
+	}
+	if err != nil {
+		http.Error(w, "load call", http.StatusInternalServerError)
+		return
+	}
+	render(w, r, DetailPage(call))
 }
 
 func (h *Handler) index(w http.ResponseWriter, r *http.Request) {
