7219fb832e9a9aba002e7ad52d7293937b464a3f
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-09-12 17:40:24 +0000

    Align tenancy module formatting

--- server/internal/tenancy/module.go
+++ server/internal/tenancy/module.go
@@ -18,13 +18,11 @@ type OrganizationModule struct {
 	Service    *organization.Service
 	Handler    *organization.Handler
 }
-
 type MembersModule struct {
 	Repository *members.Repository
 	Service    *members.Service
 	Handler    *members.Handler
 }
-
 type CredentialsModule struct {
 	Repository *credentials.Repository
 	Service    *credentials.Service
@@ -38,22 +36,9 @@ func New(queries *sqlc.Queries) *Module {
 	membersService := members.NewService(membersRepository)
 	credentialsRepository := credentials.NewRepository(queries)
 	credentialsService := credentials.NewService(credentialsRepository)
-
 	return &Module{
-		Organizations: OrganizationModule{
-			Repository: organizationRepository,
-			Service:    organizationService,
-			Handler:    organization.NewHandler(organizationService),
-		},
-		Members: MembersModule{
-			Repository: membersRepository,
-			Service:    membersService,
-			Handler:    members.NewHandler(membersService),
-		},
-		Credentials: CredentialsModule{
-			Repository: credentialsRepository,
-			Service:    credentialsService,
-			Handler:    credentials.NewHandler(credentialsService),
-		},
+		Organizations: OrganizationModule{Repository: organizationRepository, Service: organizationService, Handler: organization.NewHandler(organizationService)},
+		Members:       MembersModule{Repository: membersRepository, Service: membersService, Handler: members.NewHandler(membersService)},
+		Credentials:   CredentialsModule{Repository: credentialsRepository, Service: credentialsService, Handler: credentials.NewHandler(credentialsService)},
 	}
 }
