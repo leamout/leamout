bbdae0fd9f4f647dc9e8af7906fee71b7f01672b
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-09-12 08:58:35 +0000

    Wire prepaid authority into API runtime

--- server/internal/runtime/server/server.go
+++ server/internal/runtime/server/server.go
@@ -226,6 +226,7 @@ func NewModules(
 
 	numbersRepository := numbers.NewRepository(db, redisClient)
 	numbersService := numbers.NewService(numbersRepository)
+	numbersService.SetManagedPurchaseAuthority(commercialModule.Prepaid.Service)
 
 	sipDomainsRepository := sip_domains.NewRepository(queries)
 	sipDomainsService := sip_domains.NewService(sipDomainsRepository)
