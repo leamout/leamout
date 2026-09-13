b6e4a74c9e73f67b23860dea66ee97a886553133
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-09-13 08:15:50 +0000

    Use Self-Hosted worker composition root

--- server/cmd/selfhosted-worker/main.go
+++ server/cmd/selfhosted-worker/main.go
@@ -8,7 +8,7 @@ import (
 	"syscall"
 
 	"github.com/leamout/leamout/internal/platform/config"
-	runtimeworker "github.com/leamout/leamout/internal/runtime/worker"
+	selfhostedruntime "github.com/leamout/leamout/internal/runtime/selfhosted"
 )
 
 func main() {
@@ -24,7 +24,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	worker, err := runtimeworker.NewSelfHosted(ctx, cfg)
+	worker, err := selfhostedruntime.NewWorker(ctx, cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
