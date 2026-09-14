ed4ce116948da2e6dcb002d5b5b546b9d81ad79f
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-09-14 15:33:14 +0000

    refactor: remove redundant routing trunk mode file

--- server/internal/telecom/routing/trunk_modes.go
+++
@@ -1,9 +0,0 @@
-package routing
-
-// Trunks retain an internal provisioning mode because Cloud Managed uses a
-// Leamout-operated trunk while organization-scoped customer trunks are BYOC.
-// Phone numbers do not have this mode.
-const (
-	provisioningModeBYOC    = "byoc"
-	provisioningModeManaged = "managed"
-)
